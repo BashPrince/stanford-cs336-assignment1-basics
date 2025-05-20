b461d2d3b79719e600afdf1f7654fb599846d025
Stephen Agyemang <stephen.agyemang@hotmail.com>
2025-05-20 19:41:58 +0200

    Add pickling to main script

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import pickle
+import argparse
+from pathlib import Path
 from cs336_basics.train_bpe import train_bpe
 
 if __name__ == "__main__":
-    vocab, merge_sequence = train_bpe(input_path="data/TinyStoriesV2-GPT4-train.txt", vocab_size=10000, special_tokens=["<|endoftext|>"], num_processes=None)
+    parser = argparse.ArgumentParser(
+                    prog='Tokenizer Training',
+                    description='Tokenize an input file and store the vocab and merges to disk')
+    parser.add_argument("--corpus", required=True, type=str)
+    args = parser.parse_args()
+    
+    vocab, merge_sequence = train_bpe(input_path=args.corpus, vocab_size=10000, special_tokens=["<|endoftext|>"], num_processes=None)
+
+    file_stem = Path(args.corpus).stem
+
+    with open(f"{file_stem}-vocab.pickle", 'ab') as file:
+        pickle.dump(vocab, file=file)
+        
+    with open(f"{file_stem}-merges.pickle", 'ab') as file:
+        pickle.dump(merge_sequence, file=file)
