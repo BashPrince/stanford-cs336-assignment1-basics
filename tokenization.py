75e6ee888f711f0d3a45bd7b466c34ec2782aa15
Stephen Agyemang <stephen.agyemang@hotmail.com>
2025-05-28 12:17:01 +0200

    Periodically convert ids to np.uint16 to save memory

--- tokenization.py
+++ tokenization.py
@@ -1,5 +1,6 @@
 from cs336_basics.tokenizer import Tokenizer
 import os
+import sys
 import pickle
 import argparse
 import time
@@ -22,6 +23,8 @@ if __name__ == '__main__':
     
     tokenizer = Tokenizer(vocab, merges, special_tokens=["<|endoftext|>"])
 
+    temp_arrays = []
+
     with open(args.input, errors="replace") as f:
         # Get total file size in bytes
         f.seek(0, os.SEEK_END)
@@ -29,22 +32,34 @@ if __name__ == '__main__':
         f.seek(0)
 
         encoded_ids = []
+        total_num_ids = 0
 
         start_time = time.time()
 
         for id in  tokenizer.encode_iterable(f, file_size):
             encoded_ids.append(id)
 
+            # Periodically convert ids to np.uint16 to save memory
+            if sys.getsizeof(encoded_ids) > 1024**3:
+                encoded_ids_np = np.array(encoded_ids, dtype=np.uint16)
+                temp_arrays.append(encoded_ids_np)
+                total_num_ids += len(encoded_ids)
+                encoded_ids = []
+
         end_time = time.time()
 
         duration_sec = end_time - start_time
 
-    compression_ratio = file_size / len(encoded_ids)
+    total_num_ids += len(encoded_ids)
+    compression_ratio = file_size / total_num_ids
     throughput_mb = file_size / (duration_sec * 1024**2)
 
     print(f"Compression ratio (bytes/tokens): {compression_ratio}")
     print(f"Throughput (MB/sec): {throughput_mb}")
 
     if args.output:
-        encoded_ids_np = np.array(encoded_ids, dtype=np.uint16)
-        np.save(args.output, encoded_ids_np)
+        if encoded_ids:
+            temp_arrays.append(np.array(encoded_ids, dtype=np.uint16))
+
+        cat_array = np.concatenate(temp_arrays)
+        np.save(args.output, cat_array)
